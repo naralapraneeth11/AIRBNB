1857a0cc200005d8c075d77ba01cc4e1fc49813c
naralapraneeth11 <naralapraneeth11@gmail.com>
2026-09-24 00:25:32 -0500

    Delete src directory

--- src/app/api/[...path]/route.ts
+++
@@ -1,8 +0,0 @@
-import { handle } from "@/server/router";
-export const runtime = "nodejs";
-export const dynamic = "force-dynamic";
-export const maxDuration = 60;
-export const GET = handle;
-export const POST = handle;
-export const PATCH = handle;
-export const DELETE = handle;
