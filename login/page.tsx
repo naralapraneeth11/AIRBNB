085c878908c65e6c475411e45f54511501e08948
naralapraneeth11 <naralapraneeth11@gmail.com>
2026-09-23 23:30:36 -0500

    Delete login directory

--- login/page.tsx
+++
@@ -1,6 +0,0 @@
-import { configured } from "@/server/config";
-import { SignIn } from "@/components/sign-in";
-export const dynamic = "force-dynamic";
-export default function Login() {
-  return <SignIn configured={configured()} />;
-}
